437bd10698f6b49c37d13358f5e56bbc2bc939a6
Francisco Perez <Franciscop693@gmail.com>
2023-04-12 00:47:23 -0500

    complete lab

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const dodger = document.getElementById('dodger');
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-  
     if (left > 0) {
       dodger.style.left = `${left - 1}px`;
     }
@@ -11,7 +10,7 @@ function moveDodgerLeft() {
   function moveDodgerRight(){
     const rightNumbers = dodger.style.left.replace("px", "");
     const right = parseInt(rightNumbers, 10);
-    if(right > 0){
+    if(right > 0 && right < 360){
         dodger.style.left = `${right + 1}px`
     }
   }
